2c453ae85a4a10eb76bc8cbd9a0c3e78a6004157
Vladimir Minev <minev.dev@gmail.com>
2025-11-15 08:09:40 -0800

    Check for out file existence before subclipping in `chunker`

--- src/chunker.py
+++ src/chunker.py
@@ -26,16 +26,17 @@ def run(full_video_path: pathlib.Path, duration: int = 20, offset: int = 0) -> N
             if current_duration < duration:
                 break
 
-            clip = video.subclipped(
-                start_time=start_time, end_time=start_time + duration
-            )
-
-            current_duration -= duration
             current_video = output_path / f"{start_time}.mp4"
 
             if current_video.exists():
                 logger.info(f"{current_video!r} already exists")
                 continue
+
+            clip = video.subclipped(
+                start_time=start_time, end_time=start_time + duration
+            )
+
+            current_duration -= duration
 
             (w, h) = clip.size
             crop_width = h * 9 / 16
